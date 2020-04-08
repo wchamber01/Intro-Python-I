a4a2003d4d70a8c8e1c0049cd832d4434ee3b0d0
wchamber01 <williamchambers@gmail.com>
2020-04-08 00:18:09 -0400

    almost finished with 11_args

--- src/11_args.py
+++ src/11_args.py
@@ -7,7 +7,7 @@
 # YOUR CODE HERE
 def f1(a,b):
     return a + b
-print('f1 Value: ', f1(1, 2))
+print('f1 Value: ', f1(1, 2), 'Should print 3\n')
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum.
@@ -16,15 +16,15 @@ print('f1 Value: ', f1(1, 2))
 # YOUR CODE HERE
 def f2(*num):
     return sum(num)
-print('f2 Values: ', f2(1))                    # Should print 1
-print('f2 Values: ', f2(1, 3))                 # Should print 4
-print('f2 Values: ', f2(1, 4, -12))            # Should print -7
-print('f2 Values: ', f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
+print('f2 Values:', f2(1), 'Should print 1')                    # Should print 1
+print('f2 Values:', f2(1, 3), 'Should print 4')                 # Should print 4
+print('f2 Values:', f2(1, 4, -12), 'Should print -7')            # Should print -7
+print('f2 Values:', f2(7, 9, 1, 3, 4, 9, 0), 'Should print 33\n')  # Should print 33
 
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print('f2 Values: ', f2(*a))    # Should print 22
+print('f2 Values:', f2(*a), 'Should print 22\n')    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -32,10 +32,11 @@ print('f2 Values: ', f2(*a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-def f3(num,x=1):
-    print('f3 Values: 1 + 2 = ', f3(1, 2))  # Should print 3
-    print('f3 Values: 8 + 1 = ', f3(8))     # Should print 9
-print(f3())
+def f3(num=0,x=1):
+    return(num + x)
+print('f3 Values:', f3(1, 2), 'Should print 3')  # Should print 3
+print('f3 Values:', f3(8), 'Should print 9\n')     # Should print 9
+
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
 # prints out the keys and values like so:
@@ -46,7 +47,8 @@ print(f3())
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
-
+def f4(**info):
+    print(info.items(),'\n')
 # Should print
 # key: a, value: 12
 # key: b, value: 30
@@ -64,4 +66,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
